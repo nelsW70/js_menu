c3def71547dd1efc5dbb5d4d9cb58b06b6979cd7
Nelson Foltz <nelsfoltz@gmail.com>
2021-02-15 10:18:32 -0500

    add: create unique categories using reduce

--- app.js
+++ app.js
@@ -70,6 +70,14 @@ const menu = [
     price: 16.99,
     img: './images/item-9.jpeg',
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
+  },
+  {
+    id: 10,
+    title: 'steak dinner',
+    category: 'dinner',
+    price: 39.99,
+    img: './images/item-10.jpeg',
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`
   }
 ];
 
@@ -80,6 +88,17 @@ const filterBtns = document.querySelectorAll('.filter-btn');
 // load items
 window.addEventListener('DOMContentLoaded', function () {
   displayMenuItems(menu);
+
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ['all']
+  );
+  console.log(categories);
 });
 
 // filter items
